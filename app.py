5ec2f913d87d414adb3a9035ca06ac32bc28596f
Gaurav Gupta <gouravgupta840@gmail.com>
2024-02-11 16:35:28 +0530

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask and Docker!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
